ab392b98479b65d28c0412a381d5be9a0ace7849
Preeti Palak Kaur <75083231+preeti1605@users.noreply.github.com>
2025-09-07 20:51:28 +0530

    Update question13.sql
    answer for question 13

--- RisingTemperature/question13.sql
+++ RisingTemperature/question13.sql
@@ -45,4 +45,10 @@ In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
 In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
 **/
 
+SELECT w1.id
+FROM weather AS w1
+LEFT JOIN weather AS w2
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature
+
 
