8822e7551c2e5a743eaa2dd5eb7478ecb5d31e2d
Preeti Palak Kaur <75083231+preeti1605@users.noreply.github.com>
2025-09-07 20:53:15 +0530

    Update question14.sql
    added the answer

--- AverageTimeofProcessperMachine/question14.sql
+++ AverageTimeofProcessperMachine/question14.sql
@@ -66,3 +66,14 @@ Machine 0's average time is ((1.520 - 0.712) + (4.120 - 3.140)) / 2 = 0.894
 Machine 1's average time is ((1.550 - 0.550) + (1.420 - 0.430)) / 2 = 0.995
 Machine 2's average time is ((4.512 - 4.100) + (5.000 - 2.500)) / 2 = 1.456
 **/
+
+SELECT 
+    a1.machine_id,
+    ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+FROM activity a1
+JOIN activity a2
+    ON a1.machine_id = a2.machine_id
+    AND a1.process_id = a2.process_id
+    AND a1.activity_type = 'start'
+    AND a2.activity_type = 'end'
+GROUP BY a1.machine_id;
